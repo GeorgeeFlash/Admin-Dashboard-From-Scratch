1e238a71d76be57c21fbb4a87217130dd845197b
georgeeflash <georgeeflash@gmail.com>
2024-09-15 17:50:29 +0100

    fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,17 +18,17 @@ import Profile from "./Profile";
 const Navbar = () => {
   return (
     <nav className="bg-secondary py-4 px-5 flex  items-center justify-between text-white overflow-hidden">
+      <div className="block lg:hidden">
+        <Sheet>
+          <SheetTrigger className="bg-slate-700/80 p-1 border rounded-md">
+            <ChevronsRight className="h-7 w-7" />
+          </SheetTrigger>
+          <SheetContent className="bg-secondary" side={"left"}>
+            <Sidebar />
+          </SheetContent>
+        </Sheet>
+      </div>
       <div className="flex justify-end w-full">
-        <div className="block md:hidden">
-          <Sheet>
-            <SheetTrigger className="bg-slate-700/80 p-1 border rounded-md">
-              <ChevronsRight className="h-7 w-7" />
-            </SheetTrigger>
-            <SheetContent className="bg-secondary" side={"left"}>
-              <Sidebar />
-            </SheetContent>
-          </Sheet>
-        </div>
         <div className="flex items-center">
           <ThemeToggler />
           <DropdownMenu>
